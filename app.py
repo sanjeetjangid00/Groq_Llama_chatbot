b84994849953cf7144ed0cbc705eb330767ce7d7
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2025-01-24 10:49:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from langchain_groq import ChatGroq
 import streamlit as st
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
-os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 model = ChatGroq(model="llama-3.3-70b-versatile")
 
