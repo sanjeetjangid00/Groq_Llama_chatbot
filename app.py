cacaee6292ee8fe6a74c1a700377ee6da38177d6
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2025-08-07 12:36:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,12 +49,13 @@ for message in st.session_state['messages']:
         )
             chat_history.append(HumanMessage(content = message['content']))
         else:
-             st.markdown(
+            chat_history.append(AIMessage(content = message['content']))
+            st.markdown(
             f'<div style="text-align: left; background-color: #E0F7FA; border-radius: 15px; padding: 10px; max-width: 110%; margin: 10px 0; display: inline-block;">'
             f'<img src="https://img.icons8.com/ios/452/artificial-intelligence.png" style="vertical-align: middle; width: 25px; height: 25px; margin-right: 10px;" />'
             f'{message["content"]}</div>',
             unsafe_allow_html=True)
-            chat_history.append(AIMessage(content = message['content']))
+            
              
 if user_input:
     st.session_state['messages'].append({'role':'user', "content":user_input})
@@ -80,6 +81,7 @@ if user_input:
     )       
 
         
+
 
 
 
