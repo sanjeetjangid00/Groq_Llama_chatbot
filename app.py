5bbd9af9f9bca3dffd07951d2064de7986a5cb49
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2025-08-07 12:33:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.markdown(
 )
 # Input Section
 user_input = st.chat_input("Ask me anything...")
-
+chat_history = []
 for message in st.session_state['messages']:
         if message['role'] == 'user':
             st.markdown(
@@ -47,13 +47,15 @@ for message in st.session_state['messages']:
             f'{message["content"]}</div></div>',
             unsafe_allow_html=True
         )
+            chat_history.append(HumanMessage(content = message['content']))
         else:
              st.markdown(
             f'<div style="text-align: left; background-color: #E0F7FA; border-radius: 15px; padding: 10px; max-width: 110%; margin: 10px 0; display: inline-block;">'
             f'<img src="https://img.icons8.com/ios/452/artificial-intelligence.png" style="vertical-align: middle; width: 25px; height: 25px; margin-right: 10px;" />'
             f'{message["content"]}</div>',
             unsafe_allow_html=True
-        ) 
+        )
+            chat_history.append(AIMessage(content = message['content']))
              
 if user_input:
     st.session_state['messages'].append({'role':'user', "content":user_input})
@@ -68,7 +70,7 @@ if user_input:
 
 
     config = {"configurable": {'thread_id':1}}
-    response = chatbot.invoke({'message': [HumanMessage(content = user_input)]}, config = config)
+    response = chatbot.invoke({'message': [HumanMessage(content = chat_history)]}, config = config)
     ai_message = response['message'][-1].content
     st.session_state['messages'].append({'role':'assistant', "content":ai_message})
     st.markdown(
@@ -79,6 +81,7 @@ if user_input:
     )       
 
         
+
 
 
 
