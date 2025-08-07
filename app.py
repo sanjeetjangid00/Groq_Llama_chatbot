883c298fcf185623f2000329250dd49587ace329
SANJEET <167337847+sanjeetjangid00@users.noreply.github.com>
2025-08-07 12:46:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,14 +45,14 @@ if chat_input:
     st.session_state.messages.append({"role": "ai", "message": response.content})
 
 # Message Display Section
-for message in st.session_state.messages:
-    if message['role'] == "user":
+for msg in st.session_state.messages:
+    if msg['role'] == "user":
         # Display user message on the right with human logo
         st.markdown(
             f'<div style="display: flex; justify-content: flex-end; margin: 10px 0;">'
             f'<div style="background-color: #D3E4FF; border-radius: 15px; padding: 10px; max-width: 110%; display: flex; align-items: center;">'
             f'<img src="https://img.icons8.com/ios/452/user-male-circle.png" style="vertical-align: middle; width: 25px; height: 25px; margin-right: 10px;" />'
-            f'{message["message"]}</div></div>',
+            f'{msg["message"]}</div></div>',
             unsafe_allow_html=True
         )
     else:
@@ -60,6 +60,7 @@ for message in st.session_state.messages:
         st.markdown(
             f'<div style="text-align: left; background-color: #E0F7FA; border-radius: 15px; padding: 10px; max-width: 110%; margin: 10px 0; display: inline-block;">'
             f'<img src="https://img.icons8.com/ios/452/artificial-intelligence.png" style="vertical-align: middle; width: 25px; height: 25px; margin-right: 10px;" />'
-            f'{message["message"]}</div>',
+            f'{msg["message"]}</div>',
             unsafe_allow_html=True
         )
+
